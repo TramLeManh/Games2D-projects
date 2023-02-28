ad4bc1c9962e7b2174b0b01acf533ee94e5791c7
TramLeManh <ITITIU21077@student.hcmiu.edu.vn>
2023-02-28 08:35:05 +0700

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -6,7 +6,7 @@ public class Main {
         JFrame w = new JFrame();
         w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         w.setResizable(false);
-        w.setTitle("First 2D Games");/*Set tittle */
+        w.setTitle("The adventure of Fire Boy and Water Boy");/*Set tittle */
         GamePanel gamePanel = new GamePanel();
         w.add(gamePanel);
         w.pack();
